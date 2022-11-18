69a886120907d119f7f8e76ccc49ee9d72509cf9
aram-de <ms_aram@outlook.com>
2022-11-18 14:47:04 +0000

    Added some methods to the nuked version and their tests

--- nuked.py
+++ nuked.py
@@ -1,15 +1,16 @@
 import csv
+import sys
 
-def get_csv_and_return_as_list_of_dict(item_type) -> list:
-    list_of_dict = []
-    try:
-        with open(rf"data\{item_type}.csv", "r") as csv_file:
-            records = csv.DictReader(csv_file, skipinitialspace=True)
-            for row in records:
-                list_of_dict.append(row)
-        return list_of_dict
-    except:
-        print(rf"Failed to load data requested. Try adding one {item_type} first.")
+# def get_csv_and_return_as_list_of_dict(item_type) -> list:
+#     list_of_dict = []
+#     try:
+#         with open(rf"data\{item_type}.csv", "r") as csv_file:
+#             records = csv.DictReader(csv_file, skipinitialspace=True)
+#             for row in records:
+#                 list_of_dict.append(row)
+#         return list_of_dict
+#     except:
+#         print(rf"Failed to load data requested. Try adding one {item_type} first.")
 
 
 
@@ -22,23 +23,23 @@ class Order():
 #class item_container():
 
 #class     
-orders_list = get_csv_and_return_as_list_of_dict("orders")
+# orders_list = get_csv_and_return_as_list_of_dict("orders")
 
 
 
-couriers_list = get_csv_and_return_as_list_of_dict("couriers")
-products_list = get_csv_and_return_as_list_of_dict("products")
-oder_status_list = ['Preparing', 'Waiting for courier', 'On its way', 'Delivered', 'Delivery failed']
+# couriers_list = get_csv_and_return_as_list_of_dict("couriers")
+# products_list = get_csv_and_return_as_list_of_dict("products")
+# oder_status_list = ['Preparing', 'Waiting for courier', 'On its way', 'Delivered', 'Delivery failed']
 
-def write_list_of_dict_to_csv(item_list : list, item_type : str):
-    try:
-        with open(rf"data\{item_type}.csv", "w", newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames= item_list[0].keys())
-            writer.writeheader()
-            for dictio in item_list:
-                writer.writerow(dictio)
-    except:
-        print(rf"Failed to load data requested. Try adding one {item_type} first.")
+# def write_list_of_dict_to_csv(item_list : list, item_type : str):
+#     try:
+#         with open(rf"data\{item_type}.csv", "w", newline='') as csvfile:
+#             writer = csv.DictWriter(csvfile, fieldnames= item_list[0].keys())
+#             writer.writeheader()
+#             for dictio in item_list:
+#                 writer.writerow(dictio)
+#     except:
+#         print(rf"Failed to load data requested. Try adding one {item_type} first.")
 
 #write_list_of_dict_to_csv([ {'name' : "melon", 'age' : 3},  {'name' : "melon2", 'age' : 23}], 'orders')
 
@@ -107,12 +108,68 @@ def write_list_of_dict_to_csv(item_list : list, item_type : str):
 #     def asdict(self):
 #         return {'name': self.name, 'phone': self.phone}
 
-class itemMenu:
-    #this should be able to get an item list
+
+class Item_menu():
+    def __init__(self) -> None:
+        self.item_type = "orders"    
+        self.content = self.get_csv_and_return_as_list_of_dict()
+        self.field_names = ['customer_name', 'customer_address', 'customer_phone', 'courier', 'status', 'items' ]
+
+
+    def get_csv_and_return_as_list_of_dict(self) -> list:
+        """ as per name, if fails returns str "No file or data"""
+        list_of_dict = []
+        #print(f"trying to open data{self.item_type}.csv")
+        try:
+            with open(rf"data\{self.item_type}.csv", "r") as csv_file:
+                records = csv.DictReader(csv_file, skipinitialspace=True)
+                for row in records:
+                    list_of_dict.append(row)
+            return list_of_dict
+        except:
+            return(rf"Failed to load data requested. Try adding one {self.item_type} first.")
+        
+    #Display products -> Returns True if it returned something? False if the list is empty?   
+    
+    def show_items(self) -> None:
+        #list_of_dict = self.get_csv_and_return_as_list_of_dict()
+        list_of_dict = self.content
+        if list_of_dict == []:
+            print(f"There are no {self.item_type}s currently in the system")
+            return "No data on file or no file exist"
+        for num, line in enumerate(list_of_dict):      
+            print(f"{self.item_type[:-1].capitalize()} n.{num+1}")
+            for key, value in line.items():
+                key_4_string = key.replace("_", " ").title()
+                print(f"\t{key_4_string}: {value}")
+            print("")
+        return "File content displayed"
+
+
     #add a product to the list -> returns a list with an additional item
+    def add_item(self):
+        new_dic = {}
+        for field in self.field_names:
+            new_dic[field] = input(rf"Enter a {field}: ")
+        self.content = self.content.append(new_dic)
+        return self.content
+
+
     #select a product -> returns an index position for an item
     #delete a product from the list -> returns a list without the deleted product
     #update a product -> returns a product as dict
+
+
+    def write_list_of_dict_to_csv(self):
+        try:
+            with open(rf"data\{self.item_type}.csv", "w", newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames= self.content[0].keys())
+                writer.writeheader()
+                for dictio in self.content:
+                    writer.writerow(dictio)
+        except:
+            print(rf"Failed to load data requested. Try adding one {self.item_type[:-1]} first.")
+
 
 
 
@@ -130,35 +187,34 @@ class Menu():
         print(main_menu_string)
         try:
             u_input = int(input())
-            print("")
-            valid_choices = [num for num in range(5)]
-            print(valid_choices)
+            valid_choices = [num for num in range(4)]
             while u_input not in valid_choices:
                 print("Not a valid option. Try again.")
                 print(main_menu_string)
                 u_input = int(input())
                 print("")
             if u_input == 0:
-                return("Program ended st")
-                #exit("Program ended")
+                sys.exit(0)
             elif u_input == 1:
-                #print("Opened product menu")
+                print("Opened product menu")
                 return "Opened product menu"
                 # a_product = "Product()"
-                # a_product.item_menu()
+                #a_product.item_menu()
             elif u_input == 2:
                 print("")
-                a_courier = "Courier()"
-                a_courier.item_menu()
+                #a_courier = "Courier()"
+                a_courier = Item_menu()
             elif u_input == 3:
-                print("")
-                an_order = "Order()"
-                an_order.item_menu()
+                print("pass")
+                pass
+        except SystemExit:
+            print("Thank you for using our CLI")
+            return("Exited on purpose")
         except:
             print("Invalid input")
             return "Invalid input"
  
 
-#aCourier = Courier()
-# aMenu = Menu()
-# aMenu.menu()
+# aCourier = Courier()
+aMenu = Menu()
+aMenu.menu()
--- test_nuked.py
+++ test_nuked.py
@@ -4,39 +4,126 @@ import os
 # # # #from app4 import Courier
 from nuked import *
 
-aMenu = Menu()
 
-a_product = Product()
-a_product.item_menu()
 
+##TEST GET FILE
+
+#happy
 def test_get_file_returns_list():
-    a_list = get_csv_and_return_as_list_of_dict("orders")
+    an_item = Item_menu()
+    content = an_item.get_csv_and_return_as_list_of_dict()
+    print(content)
     expected = list
-    actual = type(a_list)
+    actual = type(content)
     assert expected == actual
 
 
 def test_get_file_returns_list_of_dict():
-    a_list = get_csv_and_return_as_list_of_dict("orders")
+    an_item = Item_menu()
+    a_list = an_item.get_csv_and_return_as_list_of_dict()
     expected = dict
     for item in a_list:
         actual = type(item) 
         assert expected == actual
 
+#unhappy
+def test_get_file_returns_when_fails():
+    an_item = Item_menu()
+    #mock data
+    a_list = []
+    expected = rf"Failed to load data requested. Try adding one {an_item.item_type} first."
+    for item in a_list:
+        actual = type(item) 
+        assert expected == actual
 
-# #Integration
 
+##TEST SHOW ITEM
+
+#happy
+def test_show_items_when_there_are():
+    an_item = Item_menu()
+    #stub data
+    an_item.content = [{'name' : "melon", 'age' : 3},  {'name' : "melon2", 'age' : 23}]
+    expected = "File content displayed"
+    actual = an_item.show_items() 
+    assert expected == actual
+
+#unhappy
+def test_show_items_when_content_empty():
+    an_item = Item_menu()
+    #stub data
+    an_item.content = []
+    expected = "No data on file or no file exist"
+    actual = an_item.show_items() 
+    assert expected == actual
+
+
+#TESTING WRITING
+
+#HAPPY
 def test_write_to_csv_writes():
     #set up
-    an_item_type = 'orders'
-    os.remove(f"data/{an_item_type}.csv") #delete file
-    aDict_list = [{'name' : "melon", 'age' : "3"},  {'name' : "melon2", 'age' : "23"}]
-    an_item_type = 'orders'
-
+    an_item = Item_menu()
+    os.remove(f"data/{an_item.item_type}.csv") #delete file so that we use mock data
+    #Mock data
+    an_item.content = [{'name' : "melon", 'age' : "3"},  {'name' : "melon2", 'age' : "23"}]
     #action
-    write_list_of_dict_to_csv(aDict_list, an_item_type)
+    an_item.write_list_of_dict_to_csv()
 
-    assert aDict_list == get_csv_and_return_as_list_of_dict("orders")
+    assert an_item.content == an_item.get_csv_and_return_as_list_of_dict()
+
+#DON'T KNOW HOW TO CREATE UNHAPPY CASE
+# def test_write_to_csv_writes_fails():
+#     """ test failes because he data passed is not a list of dict"""
+#     #set up
+#     an_item = Item_menu()
+#     os.remove(f"data/{an_item.item_type}.csv") #delete file so that we use mock data
+#     #Mock data: overwrite object content with empty list  
+#     expected =  rf"Failed to load data requested. Try adding one {an_item.item_type[:-1]} first." 
+#     an_item.content = None
+#     #action
+#     an_item.write_list_of_dict_to_csv()
+
+#     assert expected == an_item.get_csv_and_return_as_list_of_dict()
+
+
+#TESTING ADDING ITEM
+#happy
+
+def test_add_item_succeeds(monkeypatch):
+    an_item_menu = Item_menu()
+    #setup
+    #This fakes manual input
+    answers = iter(["testAdd", "3"])
+    monkeypatch.setattr('builtins.input', lambda _: next(answers, None))
+    
+    item_plus_added_content = list(an_item_menu.content).append({'name' : "testAdd", 'age' : "3"})
+    print(item_plus_added_content)
+    #action
+    assert item_plus_added_content == an_item_menu.add_item()
+
+
+def test_add_item_user_enters_nothing(monkeypatch):
+    an_item_menu = Item_menu()
+    #setup
+    #This fakes multiple manual inputs
+    manual_inputs = iter(["", ""])
+    monkeypatch.setattr('builtins.input', lambda _: next(manual_inputs, None))
+    
+    item_plus_added_content = list(an_item_menu.content).append({'name' : "testAdd", 'age' : "3"})
+    print(item_plus_added_content)
+    #action
+    assert item_plus_added_content == an_item_menu.add_item()
+
+
+
+
+
+
+
+
+
+
 
 
 def test_main_menu_invalid_input(monkeypatch):
@@ -47,31 +134,26 @@ def test_main_menu_invalid_input(monkeypatch):
     assert actual == expected
 
 
-# def test_main_menu_choose_one(monkeypatch):
-#     answers = 1
-#     monkeypatch.setattr('builtins.input', lambda : answers)
-#     actual = aMenu.menu()
-#     expected = "Opened product menu"
-#     assert actual == expected
-
-def test_main_menu_choose_one(monkeypatch, mocker):
+def test_main_menu_choose_one(monkeypatch):
     answers = 1
     monkeypatch.setattr('builtins.input', lambda : answers)
-    mocked_func = mocker.patch('test.hello')
-
     actual = aMenu.menu()
     expected = "Opened product menu"
     assert actual == expected
 
+# def test_main_menu_choose_one(monkeypatch, mocker):
+#     answers = 1
+#     monkeypatch.setattr('builtins.input', lambda : answers)
+#     mocked_func = mocker.patch('test.hello')
+
+#     actual = aMenu.menu()
+#     expected = "Opened product menu"
+#     assert actual == expected
+
 
 def test_main_menu_choose_zero(monkeypatch):
-    answers = iter([0])
-    monkeypatch.setattr('builtins.input', lambda : next(answers))
+    answers = 0
+    monkeypatch.setattr('builtins.input', lambda : answers)
     actual = aMenu.menu()
-    expected = "Program ended st"
+    expected = "Exited on purpose"
     assert actual == expected
-
-def test_hello(mocker):
-    mocked_func = mocker.patch('test.hello')
-    my_function()
-    mocked_func.assert_called_once_with('Sam')
